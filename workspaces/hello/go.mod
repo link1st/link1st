a11e8344a76e740820f42b7e3aa79145b28ec67f
link1st <562117637@qq.com>
2022-03-19 15:11:38 +0800

    add go workspaces

--- workspaces/hello/go.mod
+++ workspaces/hello/go.mod
@@ -3,5 +3,3 @@ module github.com/link1st/link1st/workspaces/hello
 go 1.18
 
 require github.com/link1st/example v0.0.0-20220318153745-5f3909db9919 // indirect
-
-replace github.com/link1st/example => ./../example
--- workspaces/hello/main.go
+++ workspaces/hello/main.go
@@ -1,36 +1,36 @@
 // Package main main 文件，go 多模块工作区演示代码
-// 实现将传入的字符串反转
+// 实现将输入的字符串反转输出并输出
 package main
 
 import (
-	"flag"
-	"fmt"
+    "flag"
+    "fmt"
 
-	"github.com/link1st/example/stringutil"
+    "github.com/link1st/example/stringutil"
 )
 
 var (
-	str = ""
+    str = ""
 )
 
 func init() {
-	flag.StringVar(&str, "str", str, "输入字符")
-	flag.Parse()
+    flag.StringVar(&str, "str", str, "输入字符")
+    flag.Parse()
 }
 
 func main() {
-	if str == "" {
-		fmt.Println("示例: go run main.go -str hello")
-		fmt.Println("str 参数必填")
-		flag.Usage()
-		return
-	}
+    if str == "" {
+        fmt.Println("示例: go run main.go -str hello")
+        fmt.Println("str 参数必填")
+        flag.Usage()
+        return
+    }
 
-	// 调用公共仓库，进行字符串反转
-	str = stringutil.Reversal(str)
-	// 将反转以后的字符串进行大写
-	str = stringutil.ToUpper(str)
-	// 输出反转后的字符串
-	fmt.Println(str)
-	return
+    // 调用公共仓库，进行字符串反转
+    str = stringutil.Reversal(str)
+    str = stringutil.ToUpper(str)
+    // 输出反转后的字符串
+    fmt.Println(str)
+    return
 }
+
